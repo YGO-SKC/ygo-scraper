708e617cd1fcb6d37da46e8434d226acac31cef8
Javi Gomez <rtomyj@gmail.com>
2021-01-18 16:03:41 -0600

    Updated script to only download images that haven't been downloaded yet

--- download-card-images.py
+++ download-card-images.py
@@ -19,16 +19,21 @@ if __name__ == '__main__':
 
     for row in dbCursor.fetchall():
         filename = f'{row[0]}.jpg'
-        img = f'https://storage.googleapis.com/ygoprodeck.com/pics_artgame/{filename}'
+        skc_img_url = f'https://yugiohsiteimages.s3.us-east-2.amazonaws.com/{filename}'
+        ygo_pro_img_url = f'https://storage.googleapis.com/ygoprodeck.com/pics_artgame/{filename}'
 
-        req = requests.get(img, stream=True)
-        if req.status_code == 200:
-            req.raw.decode_content = True
 
-            with open(f'{dir}/{filename}', 'wb') as file:
-                file.write(req.content)
-            print('Image downloaded successfully')
-        else:
-            print(f'Image {filename} - not found')
+        req = requests.get(skc_img_url)
+        if req.status_code != 200:
+            print(f'card with id {row[0]} not found in SKC, trying to fetch from ygo pro')
+            req = requests.get(ygo_pro_img_url, stream=True)
+            if req.status_code == 200:
+                req.raw.decode_content = True
 
-    db_cleanup(dbConn, dbCursor)
+                with open(f'{dir}/{filename}', 'wb') as file:
+                    file.write(req.content)
+                print('Image downloaded successfully')
+            else:
+                print(f'Image {filename} - not found')
+
+    db_cleanup(dbConn, dbCursor) # Close dB connections
